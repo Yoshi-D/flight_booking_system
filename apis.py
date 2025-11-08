4405de18ece35ac1e6b39dfcbe8119a9f90404d4
Yoshi-D <yashdhage20@gmail.com>
2025-11-09 03:01:39 +0530

    added functionality to check passenger in table

--- apis.py
+++ apis.py
@@ -58,7 +58,7 @@ async def insert_into_passenger_table(request: Request):
     conn.close()
     return {"status": "200", "passenger_id": passenger_id}
 
-@app.get("/get_flight_details")
+@app.get("/get_flight_details") #this takes in dates as well as airports
 async def get_flight_details(from_date, to_date, source_airport, destination_airport):
     #from date, to date, from which airport, to which airport
 
@@ -76,7 +76,22 @@ async def get_flight_details(from_date, to_date, source_airport, destination_air
     rows = cursor.fetchall()
     cursor.close()
     conn.close()
-    return rows
+
+    flights_data = []
+    if len(rows)>0:
+        flight = {}
+        for row in rows:
+            flight["route_id"] = row[0]
+            flight["start_ap"] = row[1]
+            flight["dest_ap"] = row[2]
+            flight["distance"] = row[3]
+            flight["schedule_id"] = row[4]
+            flight["flight_id"] = row[5]
+            flight["schedule_data"] = row[6]
+            flight["dep_time"] = row[7]
+            flight["arrival_time"] = row[8]
+            flights_data.append(flight)
+    return flights_data
 
 @app.post("/generate_ticket")
 async def insert_into_booking_table(request: Request):
@@ -120,6 +135,7 @@ async def get_passenger_ticket(passenger_id):
     query = """
                     SELECT
                     t.pnr,
+                    t.seat_number,
                     t.class_code,
                     t.price,
                     fs.schedule_date,
@@ -139,4 +155,47 @@ async def get_passenger_ticket(passenger_id):
     cursor.close()
     conn.close()
 
-    return rows
+    ticket_data = []
+    if len(rows)>0:
+        for row in rows:
+            ticket = {}
+            ticket["pnr"] = row[0]
+            ticket["seat_number"] = row[1]
+            ticket["class_code"] = row[2]
+            ticket["price"] = row[3]
+            ticket["schedule_date"] = row[4]
+            ticket["dep_time"] = row[5]
+            ticket["arrival_time"] = row[6]
+            ticket["start_ap"] = row[7]
+            ticket["dest_ap"] = row[8]
+            ticket_data.append(ticket)
+
+    return ticket_data
+
+@app.get("/check_user_exists")
+async def check_user_exists(passenger_id):
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = """
+                    select * from passenger where passenger_id = %s
+            """
+    cursor.execute(query, (passenger_id,))
+
+    row = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    passenger_data = {}
+    if row:
+        passenger_data["passenger_id"] = row[0]
+        passenger_data["gov_id_type"] = row[1]
+        passenger_data["phone_number"] = row[2]
+        passenger_data["name"] = row[3]
+        passenger_data["dob"] = row[4]
+        passenger_data["gender"] = row[5]
+        passenger_data["nationality"] = row[6]
+        passenger_data["email_address"] = row[7]
+
+    return passenger_data
+
